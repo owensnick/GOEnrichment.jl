bb776e393ff5d8b7d252708caee452519ff80cdb
owensnick <owensnick@gmail.com>
2020-05-20 20:59:44 +0100

    Files generated by PkgTemplates

---
+++ src/GOEnrichment.jl
@@ -0,0 +1,5 @@
+module GOEnrichment
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GOEnrichment
+using Test
+
+@testset "GOEnrichment.jl" begin
+    # Write your own tests here.
+end
